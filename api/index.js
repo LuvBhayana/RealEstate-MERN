a287df0190bb698da70f90a8f9c3da0e0714d1a6
luv <luvbhayana4623@gmail.com>
2024-04-07 15:28:52 +0530

    render

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to database');
 }).catch((err)=>{
     console.log(err);
 });
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +25,12 @@ app.listen(3000, ()=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Error';
