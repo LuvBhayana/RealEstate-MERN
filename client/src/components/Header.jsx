bee645e0f8ae3d0b51675fa35b8f377d9127e81c
luv <luvbhayana4623@gmail.com>
2024-04-07 15:11:12 +0530

    About page completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,10 +24,10 @@ export default function Header() {
   }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+      <div className='flex justify-between items-center max-w-8xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'>Luv</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+        <div className="about-pag">
+          <br/>
+      <h1 className= "text-slate-700 font-bold text-5xl">About Our Real Estate App</h1>
+      <br/>
+      <div className="mission-section">
+        <span className='font-semibold text-blue-400 text-2xl'>
+         
+        <h2>Our Mission</h2>
+        </span>
+        <br/>
+        <p className= "font-semibold text-slate-700  text-xl"> 
+          At the core of our app is a commitment to simplifying the real estate journey for everyone involved...
+        </p>
+        <br/>
+      </div>
+      <div className="properties-section text-slate-700">
+        <h2>About Properties</h2>
+        <p>Our real estate app offers a diverse range of properties to suit every need and preference...</p>
+      </div>
+      <div className="contact-section  text-slate-700">
+        <h2>Get in Touch</h2>
+        <p>We value your feedback and are always eager to hear from our users...</p>
+      </div>
+      <div className="team-section  text-slate-700">
+        <h2>Meet the Team</h2>
+        <p>Behind our innovative real estate app is a dedicated team of developers, designers, and real estate experts...</p>
+      </div>
+      <div className="contact-section  text-slate-700">
+        <h2>Get in Touch</h2>
+        <p>We value your feedback and are always eager to hear from our users...</p>
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <div className=' items-center mx-auto  '>
+      <button className=' bg-slate-700 text-white p-4  mx-auto items-center uppercase rounded-lg dive-in border'>
+      <Link
+          to={"/Signup"}
+        >
+          Dive-in
+        </Link>
+         
+      </button>
+    </div>
+    </div>
   )
 }
